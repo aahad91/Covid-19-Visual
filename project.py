dcb5342a0d96396832bd41bc1bdcaa8b5af47b0e
aahad91 <abdulahadayaz@hotmail.com>
2020-06-14 18:13:34 +0200

    added project.py file

---
+++ project.py
@@ -0,0 +1,18 @@
+"""
+Assignment-5: Covid-19 Visualization
+
+
+Group-11
+Members: Abdul Ahad Ayaz, Sayalee Chavan, Suganthi Jaganathan,
+Bhagyashree Sanjay Borade
+"""
+
+# ---packages--- #
+import numpy as np
+import pandas as pd
+
+# ---Task Functions --- #
+
+
+# --------------------- #
+if __name__ == "__main__":
